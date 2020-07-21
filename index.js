06a788f2bc4c97862cd5a16e21a189fcb8decd36
Kinshuk-Goel <67846964+Kinshuk-Goel@users.noreply.github.com>
2020-07-20 23:05:18 -0400

    Quote update
    Gif added to !boomer_alert

--- index.js
+++ index.js
@@ -63,6 +63,10 @@ client.on('message', message => {
 		message.channel.send('https://images-ext-2.discordapp.net/external/qZw1eKzoZ5YQxVjRpzs61DBuGvbzvO0wBRtvG0VpOp4/https/images-ext-1.discordapp.net/external/DUyLzeMzo93_divTJPzzGVzsdJfcWZ17kfUlrtyx3gY/https/media.tenor.com/images/8de5bce8f5903037ada38cce0157738f/tenor.gif');
 	}
 
+	if (command === 'boomer_alert') {
+		message.channel.send('https://images-ext-1.discordapp.net/external/FzIHi9fXAmW6_jOhK5drP9C8sCmCaBTOp2NkbzUd3s8/https/media.tenor.com/images/f7c2d344e67d5fb01e05804b17dac783/tenor.gif');
+	}
+
 	
 
 // I added a new label and i editied it
