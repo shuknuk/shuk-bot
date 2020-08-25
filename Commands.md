88c768b76d9d83201c282134f7d865a8ab516988
Kinshuk-Goel <67846964+Kinshuk-Goel@users.noreply.github.com>
2020-08-25 12:02:39 -0400

    updated commands.md & index.js
    yee

--- Commands.md
+++ Commands.md
@@ -23,6 +23,45 @@ For example, if you do:
 then it will delete 12 messages in your discord server. BE CAREFUL OF THESE COMMANDS
 Limit is 2-100 per text submission. 
 
+Emoji Reactions:
+//  !happy
+//  !like
+//  !love
+//  !haha
+//  !wow
+//  !sad
+//  !mad
+//  !XD
+//  !mask
+//  !yes
+//  !no
+
+// Specific meme stuff (include gifs) and other stuff:
+//  !tacosCommands to use for Discord Bot
+
+Note: All commands used for the Discord Bot start with => !
+
+// List of commands:
+//  !args-info here are my arguments 
+//  !args-info foo |  !args-info not-foo 
+//  !kick @User
+//  !avatar 
+//  !avatar @User
+//  !author
+//  !server
+
+DANGEROUS COMMANDS:
+
+//  !prune (amount)
+
+When using this command, it will delete the amount of discord messages in the chat. 
+For example, if you do:
+
+    !prune 12
+
+then it will delete 12 messages in your discord server. BE CAREFUL OF THESE COMMANDS
+Limit is 2-100 per text submission. 
+
 
 Emoji Reactions:
 `		//  !happy
@@ -50,5 +89,8 @@ Emoji Reactions:
 //  !boomer_alert
 //  !apple
 //  !android
+//  !youtube
+//  !mornin
+
 //
 //
--- index.js
+++ index.js
@@ -22,12 +22,13 @@ client.on('message', message => {
 	
 
 	// ALL COMMANDS TO USE WITH BOT
+
 	if (command === 'list') {
-		message.channel.send(' `!hello` `!help` `!kick @User` `!avatar @user` `!prune (amount) [dis command deletes msgs so be careful]` `!author` `!server`  `Emojis => !happy !like !love !haha !wow !sad !mad !XD !mask !yes !no` ');
+		message.channel.send(' `!hello` `!help` `!kick @User` `!avatar @user` `!prune (amount) [dis command deletes msgs so be careful]` `!author` `!server`  `Emojis => !happy !like !love !haha !wow !sad !mad !XD !mask !yes !no`  More meme commands => `!tacos !cats !github !apple_vs_android !dogs_terrified !doit !google !panic !boomer_alert !apple !android !youtube !mornin ` ');
 	}
 	// END OF COMMANDS TO USE WITH BOT
 
-	// ---------------------------------SEPERATION--------------------------------------
+	// ---------------------------------SEPERATION------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     if (command === 'tacos') {
         message.channel.send('https://tenor.com/view/tacos-taco-guru-hartos-tacos-toma-pobre-thrown-gif-12143253');
@@ -46,7 +47,7 @@ client.on('message', message => {
 	}
 
 	if (command === 'hello') {
-		message.channel.send('Hello there! Type !help to learn what commands to use with me!');
+		message.channel.send('Hello there! Type !list to learn what commands to use with me! You can also type !help to view a simplified page of the commands to use.');
 	}
 
 	if (command === 'author') {
