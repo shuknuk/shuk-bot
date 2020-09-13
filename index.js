683ba3012f43c7acca5caf6c57d189397da3ff4a
Kinshuk Goel <67846964+Kinshuk-Goel@users.noreply.github.com>
2020-09-13 11:43:43 -0400

    + Organized the list commands

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ client.on('message', message => {
 
 	// ALL COMMANDS TO USE WITH BOT
 	if (command === 'list') {
-		message.channel.send(' `!hello` `!help` `!kick @User` `!avatar @user` `!prune (amount) [dis command deletes msgs so be careful]` `!author` `!server`  `Emojis => !happy !like !love !haha !wow !sad !mad !XD !mask !yes !no` ||| `!tacos !cats !github !apple_vs_android !reddit !yo (warning this has very loud annoying audio) !fireworks !dogs_terrified !poop !doit !google !cheers !panic !news !boomer_alert !apple !roll !knuckles !bored !bye !android !youtube !mornin` ');
+		message.channel.send(' There are two lists, `!list` and `!list2`              `!hello` `!help` `!kick @User` `!avatar @user` `!prune (amount) [dis command deletes msgs so be careful]` `!author` `!server`  `Emojis => !happy !like !love !haha !wow !sad !mad !XD !mask !yes !no` ||| `!tacos !cats !github !apple_vs_android !reddit !yo (warning this has very loud annoying audio) !fireworks !dogs_terrified !poop !doit !google !cheers !panic !news !boomer_alert !apple !roll !knuckles !bored !bye !android !youtube !mornin` ');
 	}
 	// END OF COMMANDS TO USE WITH BOT
 
@@ -119,7 +119,7 @@ client.on('message', message => {
 		message.channel.send('Bye https://thumbs.gfycat.com/ClumsyFarawayBeardedcollie-size_restricted.gif');
 	}
 
-	if (command === '@DaShukBot how are you doing?') {
+	if (command === 'DaShukBot how are you doing?') {
 		message.channel.send('I am doing good, thanks for asking! How are you doing?');
 	}
 
