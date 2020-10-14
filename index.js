fa4dd8fc7d8a14da6f95bf085a406acefe5d3669
Kinshuk Goel <67846964+Kinshuk-Goel@users.noreply.github.com>
2020-10-14 18:22:56 -0400

    + Added new commands + !why and !dead

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ client.on('message', message => {
 
 	// ALL COMMANDS TO USE WITH BOT
 	if (command === 'list') {
-		message.channel.send(' There are two lists, `!list` and `!list2` You are currently viweing List 1 (!list)              `!hello` `!help` `!kick @User` `!avatar @user` `!prune (amount) [dis command deletes msgs so be careful]` `!author` `!server`  `Emojis => !happy !like !love !haha !wow !sad !mad !XD !mask !yes !no` ||| `!tacos !cats !github !apple_vs_android !dingdong !reddit !yo (warning this has very loud annoying audio) !fireworks !dogs_terrified !poop !doit !google !cheers !panic !news !boomer_alert !apple !roll !knuckles !thanks !bored !bye !minecraft !android !youtube !school !mornin` ');
+		message.channel.send(' There are two lists, `!list` and `!list2` You are currently viweing List 1 (!list)              `!hello` `!help` `!kick @User` `!avatar @user` `!prune (amount) [dis command deletes msgs so be careful]` `!author` `!server`  `Emojis => !happy !like !love !haha !wow !sad !mad !XD !mask !yes !no` ||| `!tacos !cats !github !apple_vs_android !dingdong !reddit !yo (warning this has very loud annoying audio) !fireworks !dogs_terrified !poop !doit !google !cheers !panic !news !boomer_alert !apple !roll !why !dead !knuckles !thanks !bored !bye !minecraft !android !youtube !school !mornin` ');
 	}
 
 	if (command === 'list2') {
@@ -49,6 +49,10 @@ client.on('message', message => {
 
 	if (command === 'school') {
 		message.channel.send('https://media.tenor.com/images/7a2a78b065e7be080232b379e9ff0cc4/tenor.gif');
+	}
+
+	if (command === 'dead') {
+		message.channel.send('https://media3.giphy.com/media/QNGtIbsXz88V2/giphy.gif');
 	}
 
 	if (command === 'thanks') {
